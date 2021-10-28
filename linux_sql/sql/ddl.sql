d4c31e19ba03df95878f3ba2f7fbc3865d7de3d8
Varun Kumar Vats <vatsvarun782@gmail.com>
2021-10-28 22:13:29 +0000

    sql tables

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,28 @@
+CREATE TABLE PUBLIC.host_info 
+  ( 
+     id               SERIAL NOT NULL PRIMARY KEY, 
+     hostname         VARCHAR NOT NULL, 
+     cpu_number       INT     NOT NULL,
+     cpu_architecture INT     NOT NULL,
+     cpu_model        VARCHAR NOT NULL,
+     cpu_mhz          INT     NOT NULL,
+    L2_cache          INT     NOT NULL,
+    total_mem         INT     NOT NULL,
+    timestamp         TIMESTAMP NOT NULL
+
+  );
+
+
+CREATE TABLE PUBLIC.host_usage 
+  ( 
+     "timestamp"      TIMESTAMP NOT NULL, 
+     host_id           SERIAL    NOT NULL PRIMARY KEY,
+     memory_free      INT       NOT NULL,
+     cpu_idle         INT       NOT NULL,
+     cpu_kernel       INT      NOT NULL,
+    disk_io           INT      NOT NULL,
+   disk_availaible    INT     NOT NULL
+ 
+  );
+
+
