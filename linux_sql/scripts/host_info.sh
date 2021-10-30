39500f4f8aaa5af8518426c266a0ddbc2fa80b22
Varun Kumar Vats <vatsvarun782@gmail.com>
2021-10-30 02:02:09 +0000

    usage info

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,32 +1,33 @@
+#!/bin/bash
 
-#save number of CPU to a variable
-lscpu_out=`lscpu`
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 
-#hardware
-hostname= hostname -f
-cpu_number= echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs
-cpu_number= echo "$lscpu_out"  | egrep "^Architecture\:" | awk '{print $2}' | xargs
+#Error message for missing arguments
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters. Please provide psql host, port number, database name, user and password"
+    exit 1
+fi
 
-cpu_model= grep -m 1 'model name' /proc/cpuinfo
-cpu_mhz= grep -m 1 'cpu MHz' /proc/cpuinfo
-total_memory= grep -m 1 'Total_mem' /proc/meminfo
-timestamp= vmstat -t  | awk '{print $18,$19}'| tail -n1 | xargs
+hostname=$(hostname -f)
+lscpu_out=$(lscpu)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep 'CPU MHz' | awk '{print $3 }' | xargs)
+l2_cache1=$(echo "$lscpu_out"  | egrep 'L2 cache' | awk '{print $3 }' | xargs)
+l2_cache=${l2_cache1//[^[:digit:].-]/}
+total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}'| xargs)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-insert_stmt="INSERT INTO host_info (hostname,cpu_number,cpu_architecture,cpu_model,cpu_mhz,total_memory,"timestamp")"
+#Query to insert data into host_info table
+insert_stmnt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', $cpu_number, '$cpu_architecture', $cpu_model, $cpu_mhz, $l2_cache, $total_mem, '$timestamp');"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Executing Insert statement through psql CLI tool
+export PGPASSWORD=$psql_password
+psql -h localhost -p 5432 -U postgres -d host_agent -c "$insert_stmnt"
+exit 0
 
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,31 +1,28 @@
-
-
- memory_free= vmstat --unit M | awk '{print $4}'| tail -n1 | xargs
- cpu_idle= vmstat --unit M| awk '{print $15}'| tail -n1 | xargs
- cpu_kernel= vmstat --unit M | awk '{print $13}'| tail -n1 | xargs
- disk_io= vmstat -d | awk '{print $10}'| tail -n1 | xargs
- disk_available= df -BM / | awk '{print $4}'
- timestamp= vmstat -t  | awk '{print $18,$19}'| tail -n1 | xargs
-
-
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
-
-insert_stmt="INSERT INTO host_usage(timestamp,memory_free,cpu_idle,cpu_kernel,disk_io,disk_availaible) VALUES('$timestamp', '$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_availaible')"
-
-export PGPASSWORD=$psql_password
-
-psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
-exit $?
-
-
-
-
-
-
-
-
-
-
+#!/bin/bash
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+#Error message for missing arguments
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters. Please provide psql host, port number, database name, user and password"
+    exit 1
+fi
+hostname=$(hostname -f)
+vmstat_mb=$(vmstat --unit M)
+memory_free=$(vmstat --unit M  |  awk '{print $4}' |tail -n1|  xargs)
+cpu_idle=$(vmstat --unit M |  awk '{print $15}' | tail -n1| xargs)
+cpu_kernel=$(vmstat --unit M |  awk '{print $13}' |tail -n1| xargs)
+disk_io=$( vmstat -d  |  awk '{print $10 }' | tail -n1| xargs)
+disk_availaible=$( df -BM / | awk '{print $4}'| tail -n1|xargs)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+#Query to insert data into host_usage table
+insert_stmt="INSERT INTO host_usage (timestamp,host_id,memory_free,cpu_idle,cpu_kernel,disk_io,disk_availaible) VALUES('$timestamp',(SELECT id FROM host_info WHERE hostname='$hostname'),'$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_availaible')"
+#Executing Insert statement through psql CLI tool
+export PGPASSWORD=$5
+psql -h localhost -p 5432 -U postgres -d host_agent -c "$insert_stmt"
+exit 0
 
 
 
