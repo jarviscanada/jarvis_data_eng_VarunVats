08b3f8e15ce6704d257cb8a776e7dd1def3f01e3
Varun Kumar Vats <vatsvarun782@gmail.com>
2021-10-30 01:58:10 +0000

    sql data

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,9 +1,11 @@
+
+
 CREATE TABLE PUBLIC.host_info 
   ( 
      id               SERIAL NOT NULL PRIMARY KEY, 
      hostname         VARCHAR NOT NULL, 
      cpu_number       INT     NOT NULL,
-     cpu_architecture INT     NOT NULL,
+     cpu_architecture VARCHAR     NOT NULL,
      cpu_model        VARCHAR NOT NULL,
      cpu_mhz          INT     NOT NULL,
     L2_cache          INT     NOT NULL,
@@ -16,13 +18,16 @@ CREATE TABLE PUBLIC.host_info
 CREATE TABLE PUBLIC.host_usage 
   ( 
      "timestamp"      TIMESTAMP NOT NULL, 
-     host_id           SERIAL    NOT NULL PRIMARY KEY,
+     host_id           SERIAL   NOT NULL ,
      memory_free      INT       NOT NULL,
      cpu_idle         INT       NOT NULL,
      cpu_kernel       INT      NOT NULL,
     disk_io           INT      NOT NULL,
-   disk_availaible    INT     NOT NULL
+   disk_availaible    BYTEA     NOT NULL,
+   
+   CONSTRAINT fk_host
+      FOREIGN KEY(host_id) 
+	  REFERENCES PUBLIC. host_info(id)
  
-  );
-
+ );
 
