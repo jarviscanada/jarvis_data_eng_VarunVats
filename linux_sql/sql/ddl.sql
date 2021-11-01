2fd464d581f7acc5cc342b3e7359d229ca8e38d0
Varun Kumar Vats <vatsvarun782@gmail.com>
2021-11-01 18:39:28 +0000

    sql query

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,32 +1,28 @@
-CREATE TABLE host_info
-  (
-     id               SERIAL NOT NULL PRIMARY KEY,
-     hostname         VARCHAR NOT NULL,
-     cpu_number       INT     NOT NULL,
-     cpu_architecture VARCHAR     NOT NULL,
-     cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          INT     NOT NULL,
-    L2_cache          BYTEA     NOT NULL,
-    total_mem         INT     NOT NULL,
-    timestamp         TIMESTAMP NOT NULL
+--connect to host agent
+\c host_agent;
 
-  );
+--Creates host_info table only if it does not exists to store hardware specifications
+CREATE TABLE iF NOT EXISTS public.host_info
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	hostname VARCHAR NOT NULL UNIQUE,
+	cpu_number INT NOT NULL,
+	cpu_architecture VARCHAR NOT NULL,
+	cpu_model INT NOT NULL,
+	cpu_mhz  INT NOT NULL,
+	L2_cache VARCHAR NOT NULL,
+	total_mem INT NOT NULL,
+	"timestamp" TIMESTAMP NOT NULL
+);
 
-
-CREATE TABLE host_usage
-  (
-     timestamp      TIMESTAMP NOT NULL,
-     host_id           SERIAL   NOT NULL PRIMARY KEY,
-     memory_free      INT       NOT NULL,
-     cpu_idle         INT       NOT NULL,
-     cpu_kernel       INT      NOT NULL,
-    disk_io           INT      NOT NULL,
-    disk_availaible    BYTEA     NOT NULL,
-
-
-
-     CONSTRAINT fk_info
-     FOREIGN KEY (host_id)
-     REFERENCES host_info (id)
-  );
-
+--Creates host_usage table only if it does not exist to store resource usage data
+CREATE TABLE if NOT EXISTS PUBLIC.host_usage
+(
+"timestamp" TIMESTAMP NOT NULL,
+host_id SERIAL NOT NULL REFERENCES host_info(id),
+memory_free INT NOT NULL,
+cpu_idle INT NOT NULL,
+cpu_kernel INT NOT NULL,
+disk_io INT NOT NULL,
+disk_available INT NOT NULL
+);
