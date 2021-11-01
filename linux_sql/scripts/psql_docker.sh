d191534938796db22cea27fe0fd884e2aa205cfd
Varun Kumar Vats <vatsvarun782@gmail.com>
2021-11-01 19:13:52 +0000

    psql_docker.sh script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,34 +1,65 @@
-#!/bin/bash
+#! /bin/sh
+
+#CLI Arguments
+cmd=$1
+db_username=$2
+db_password=$3
+
 #Start docker
 sudo systemctl status docker || systemctl start docker
-#check container status. If container exists then $exit==2
-if [[ $1 == "start" ]] | [[ $1 == "stop" ]] | [[ $1 == "create" ]]; then
-case $1 in
-start)
-    if [[ "$exists" == 2 ]]; then
-      docker start "$2"
-    else
-      echo "Cannot able to start the specified container as it is not created"
-    fi
-    exit $?
-  ;;
- create)
-    if [[ "$exists" == 2 ]]; then 
-      echo "The container already exists!"
-    else
-       #creates psql container with the given username and password
-        docker pull postgres
-        docker volume create pgdata
-	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-	docker container ls -a -f name=jrvs-psql
-	docker ps -f name=jrvs-psql
-	sudo yum install -y postgresql
-	export PGPASSWORD='password'
-	psql -h localhost -U postgres -d postgres -W
-    else 
-        echo "username and password is missing"
-      fi
-      exit $?
-    fi
+
+#check container status (try the following cmds on terminal)
+docker container inspect jrvs-psql
+container_status=$?
+
+#User switch case to handle create|stop|start opetions
+case $cmd in 
+  create)
+  
+  # Check if the container is already created
+  if [ $container_status -eq 0 ]; then
+		echo 'Container already exists'
+		exit 1	
+	fi
+
+  #check # of CLI arguments
+  if [ $# -ne 3 ]; then
+    echo 'Create requires username and password'
     exit 1
-  ;; 
+  fi
+  
+  #Create container
+        docker pull postgres
+	docker volume create pgdata
+        export PGPASSWORD='password'
+	docker run docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+
+#check if the container is created
+docker container ls -a -f name=jrvs-psql
+
+#check if container is running
+docker ps -f name=jrvs-psql
+
+	exit $?
+	;;
+
+  start|stop) 
+  #check instance status; exit 1 if container has not been created
+  if [ $container_status -ne 0];then
+       echo 'Container does not exist'
+
+  exit 1
+
+  fi
+
+  #Start or stop the container
+	docker container $cmd jrvs-psql
+	exit $?
+	;;	
+  
+  *)
+	echo 'Illegal command'
+	echo 'Commands: start|stop|create'
+	exit 1
+	;;
+esac 
