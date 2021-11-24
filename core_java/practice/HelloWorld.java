316e620a6d11ba54a4913b6c4fe6f89474b41666
Varun Vats <vatsvarun782@gmail.com>
2021-11-23 21:05:31 -0500

    Grep App code

---
+++ core_java/practice/HelloWorld.java
@@ -0,0 +1,35 @@
+package ca.jrvs.apps.practice;
+
+classHelloWorld {
+	public static void main(String args[]){
+		System.out.println("Hello, World");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
